78cebc565a5f2061ccac3a528c6f66d61da6d4ee
angel <angel@ANGELO>
2024-03-24 19:36:51 -0300

    Adicionar arquivos de projeto.

---
+++ ecouni_site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ecouni_site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
